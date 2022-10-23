fcc26861ecd72b7977dfe122f0593bc0d8932d5d
animeshm1995 <animesh.mukul@gmail.com>
2022-10-23 14:05:47 -0400

    Assignment 6: changed tuiter landing page to home page

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,9 +1,17 @@
 import "./index.css";
 import PostList from "../post-list";
+import PostSummaryList from "../post-summary-list";
 const HomeComponent = () => {
     return(
         <>
-            <PostList/>
+            <div className="row mt-2">
+                <div className="col-10 col-lg-8 col-xl-7">
+                    <PostList/>
+                </div>
+                <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-5">
+                    <PostSummaryList/>
+                </div>
+            </div>
         </>
     );
 };
